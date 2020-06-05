a5544ee7da3b067f8a949a87566d8e7658bf03a6
gennadiy123 <shelukhin_g@ukr.net>
2020-06-05 07:46:19 +0300

    homework-3

---
+++ 03-mongodb/index.js
@@ -0,0 +1,20 @@
+const usersRouter = require("./user.router");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = 3000;
+
+const server = express();
+
+mongoose.connect("process.env.MONGODB_URL", { useNewUrlParser: true });
+
+server.use(express.json());
+server.use("/api/contacts", usersRouter);
+
+server.use((err, req, res, next) => {
+  return res.status(err.status).send(err.message);
+});
+
+server.listen(PORT, () => {
+  console.log("Server started listening on port", PORT);
+});
